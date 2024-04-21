f76723ef8153142b6803b1ba0ad2376134c60930
YevhenBez <115114239+YevhenBez@users.noreply.github.com>
2024-04-21 19:13:39 +0000

    Deploying to gh-pages from @ YevhenBez/use-mongodb-for-login-signup@92bb050bad1266eb0947021dff0c669775429580 🚀

---
+++ static/js/518.eae89f24.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[518],{518:function(e,t,n){n.r(t),n.d(t,{default:function(){return s}});var r=n(184),s=function(){return(0,r.jsx)("div",{style:{height:"100vh",justifyContent:"center",alignItems:"center",fontSize:40,color:"#010101"},children:(0,r.jsx)("h2",{children:"The development of these pages is not included in this test task."})})}}}]);
+//# sourceMappingURL=518.eae89f24.chunk.js.map
